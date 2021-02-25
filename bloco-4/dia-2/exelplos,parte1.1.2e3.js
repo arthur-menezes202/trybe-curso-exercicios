178d116404595924a8a6801fdc14f195fd951f28
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-02-25 15:08:36 -0300

    resolvendo o ultimo exemplo

--- bloco-4/dia-2/exelplos,parte1.1.2e3.js
+++ bloco-4/dia-2/exelplos,parte1.1.2e3.js
@@ -5,7 +5,13 @@ console.log(menuServices);*/
 
 // exemplo 2
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
-console.log(indexOfPortfolio);
+console.log(indexOfPortfolio);*/
+
+//exemplo 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("Contato")
+console.log(menu);
