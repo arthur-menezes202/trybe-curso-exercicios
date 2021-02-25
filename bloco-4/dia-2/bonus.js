a892a04051529c8d2748bd69f6b082630070432f
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-02-25 19:08:21 -0300

    ultimo commit, tentei fazer o bonus mas não consegui

---
+++ bloco-4/dia-2/bonus.js
@@ -0,0 +1,12 @@
+let array=[] 
+for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[i] < array[j]) {
+        let position = array[i];
+  
+        array[i] = array[j];
+        array[j] = position;
+      }
+      console.log(array)
+    }
+  }
