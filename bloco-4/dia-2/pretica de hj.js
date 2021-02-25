61c2b2c25702a06a50d078b3b73fd44e735863c7
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-02-25 18:19:30 -0300

    exercicio 2 e 3

--- bloco-4/dia-2/pretica de hj.js
+++ bloco-4/dia-2/pretica de hj.js
@@ -1,3 +1,22 @@
-
+/*exercicio 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 console.log(numbers);
+*/
+
+/*exercicio 2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let cont = 0;
+for(indexPlay = 0; indexPlay < numbers.length; indexPlay ++){
+     cont = cont + numbers[indexPlay];
+}
+console.log(cont);
+*/
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let cont = 0;
+for(indexPlay = 0; indexPlay < numbers.length; indexPlay ++){
+     cont = cont + numbers[indexPlay];
+}
+let media = cont / numbers.length;
+console.log(media);
