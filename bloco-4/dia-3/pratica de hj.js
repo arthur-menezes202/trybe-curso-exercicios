6388e47e55c01c7b85ee38616675c6734515bd80
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-02-26 16:08:02 -0300

    2 questao, foi complicado mas consegui

--- bloco-4/dia-3/pratica de hj.js
+++ bloco-4/dia-3/pratica de hj.js
@@ -1,3 +1,4 @@
+/* exercicio 1
 let n = 5;
 let array = [];
 for(let i = 0;i < n;i ++){
@@ -5,4 +6,18 @@ for(let i = 0;i < n;i ++){
 }
 for(i = 0;i < n;i ++){
     console.log(array);
+}
+*/
+
+
+let n = 5;
+let array = ["*"];
+for(let j = 1;j < n + 1;j ++){ 
+    for(let i = 0;i < j;i ++){
+        array[i]="*"
+    }
+    
+    console.log(array);
+   
+
 }
