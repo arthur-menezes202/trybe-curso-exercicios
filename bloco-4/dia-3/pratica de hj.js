976ec010d7cea0d29bdbcd02301a06b63d377d97
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-02-26 17:54:16 -0300

    tarefa 3,ps: meio complicada

--- bloco-4/dia-3/pratica de hj.js
+++ bloco-4/dia-3/pratica de hj.js
@@ -9,15 +9,29 @@ for(i = 0;i < n;i ++){
 }
 */
 
-
+/* exercicio 2
 let n = 5;
 let array = ["*"];
 for(let j = 1;j < n + 1;j ++){ 
     for(let i = 0;i < j;i ++){
         array[i]="*"
     }
-    
     console.log(array);
-   
-
 }
+*/
+
+/*exercicio 3
+let n = 5;
+let array = [];
+
+    for(let i = 0;i < n;i ++){
+        array[i]=" "
+    }
+   
+n=n-1;
+    for( i = n;i >= 0;i --){
+        array[i]="*"
+        console.log(array)
+    }
+*/
+
