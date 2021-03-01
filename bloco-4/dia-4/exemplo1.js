b0d5cc7bc6680c766b033f6060f5cfe75ea6eee5
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-01 18:16:47 -0300

    ultimo exemplo

--- bloco-4/dia-4/exemplo1.js
+++ bloco-4/dia-4/exemplo1.js
@@ -8,8 +8,19 @@ let player = {
 /* exemplo 1
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade");
 */
-
+/* exemplo 2
 let bestInTheWorld = {
     medalhas: [2006, 2007, 2008, 2009, 2010, 2018]
 }
 console.log("A jogadora Marta Silva foi eleita a melhor do mundo por " + bestInTheWorld.medalhas.length + " vezes");
+*/ 
+
+let car = {
+    type: "Fiat",
+    model: "500",
+    color: "white",
+  };
+  
+  for (let i in car) {
+    console.log(i ,car[i]);
+  }
