b78b44826c7d960f87143753b569444f100e69a5
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-01 18:34:44 -0300

    exercicio 4

--- bloco-4/dia-4/pratica1.js
+++ bloco-4/dia-4/pratica1.js
@@ -28,3 +28,16 @@ let info = {
 
   console.log(Object.keys(info));
 */
+
+/* exercicio 4
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+  info.recorrente ="sim";
+
+  for(let key in info){
+      console.log(info[key])
+  } 
+  */
