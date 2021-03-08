50c71d98cc952fd3997fcf0802b369c6e952f73f
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-08 15:38:14 -0300

    tentando a segunda bloco-5-dia-4

--- bloco-5/dia-4/pratica de hj js.js
+++ bloco-5/dia-4/pratica de hj js.js
@@ -28,9 +28,21 @@ function mudaCorTexto(){
     //     body.style.backgroundColor = localStorage.getItem("cor");
     // }
 }
+ let encontraTexto = document.getElementsByClassName("texto");
+ console.log(encontraTexto[0])
 function mudaTamanhoDoTexto() {
-    let pegaP =  document.body.style.fontSize = "18px";
-}
+
+    let pegaP =  document.getElementsByTagName("p")[0];
+    pegaP.style.fontSize = localStorage.getItem("tamanho");
+    if (pegaP.style.fontSize != "20px"){
+    localStorage.setItem("tamanho" , "20px");
+    console.log("entrou no if");
+    } 
+    else {
+        localStorage.setItem("tamanho" , "15px"); 
+        console.log("entrou no else");
+    }
+} 
 function mudaEspacamento() {
     console.log("teste 4 ");
 }
