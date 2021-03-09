cd1670a4c2ad85e34b65a91a22dde12f73500444
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-09 12:51:58 -0300

    bloco-5-dia-4 finalizando tudo

--- bloco-5/dia-4/pratica de hj js.js
+++ bloco-5/dia-4/pratica de hj js.js
@@ -17,21 +17,22 @@ function mudaCorDoFundo(){
 
 
 function mudaCorTexto(){
-    // if (mudaCor.style.backgroundColor != 'red') {
-    //     localStorage.setItem("cor" , "red");
-    //     body = document.body;
-    //     body.style.backgroundColor = localStorage.getItem("cor");
-    // }
-    // else {
-    //     localStorage.setItem("cor" , "white");
-    //     body = document.body;
-    //     body.style.backgroundColor = localStorage.getItem("cor");
-    // }
+    let  pegaTodoOtexto = document.getElementsByClassName("texto")[0];
+    let pegaCor = pegaTodoOtexto.style.color;
+    if(pegaCor !== "blue"){
+        localStorage.setItem("corTexto" , "blue");
+        mudaTexto();
+    }
+    else {
+        localStorage.setItem("corTexto" , "black");
+        mudaTexto();
+    }
 }
  function mudaTexto(){
     let encontraTexto = document.getElementsByClassName("texto");
     for(let index = 0; index < encontraTexto.length;index ++){
     encontraTexto[index].style.fontSize = localStorage.getItem("tamanho");
+    encontraTexto[index].style.color = localStorage.getItem("corTexto");
  }
   
  } 
@@ -43,20 +44,19 @@ function mudaTamanhoDoTexto() {
     if (tamanhoDaFonte !== '15px'){
     localStorage.setItem("tamanho" , "15px");
     mudaTexto();
-    console.log("entrou no if");
     } 
     else {
         localStorage.setItem("tamanho" , "20px"); 
         mudaTexto();
-        console.log("entrou no else");
     }
-    console.log(tamanhoDaFonte);
     
 } 
 
 
 function mudaEspacamento() {
-    console.log("teste 4 ");
+    let PegaTexto = document.getElementsByClassName("texto")[0];
+    let espacamento = PegaTexto.style.line;
+    console.log(espacamento);
 }
 function mudaTipoDeFonte() {
     console.log("teste 5");
