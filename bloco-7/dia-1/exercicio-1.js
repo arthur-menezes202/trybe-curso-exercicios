0a531ba7fd75a3881be307643bd29c151da72f07
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-23 16:07:10 -0300

    exercicio 1 bloco-7.1

---
+++ bloco-7/dia-1/exercicio-1.js
@@ -0,0 +1,23 @@
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//  }
+
+//  testingScope(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  oddsAndEvens[0] = 2 
+  oddsAndEvens[1] = 7
+  oddsAndEvens[2] = 10
+  oddsAndEvens[3] = 4
+  oddsAndEvens[4] = 3
+  oddsAndEvens[5] = 13
+
+  console.log(oddsAndEvens);
