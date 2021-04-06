b1e2f39fc28f3ef1e347a619b727fd936045a04a
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-06 18:41:04 -0300

    inicialdo exercicio 8.4

---
+++ bloco-8/dia-4/exercicio-1.js
@@ -0,0 +1,16 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+function flatten(array) {
+  return array.reduce((acumulador, index) => {
+      return `${acumulador}, ${index}`;
+  })
+}
+console.table(flatten(arrays));
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
